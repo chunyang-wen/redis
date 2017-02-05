595ea51cc6c63150cf8bd8c09ae07c67f725e5af
chunyang <chunyang.wen@gmail.com>
2017-02-05 05:55:45 -0800

    comment on crc64.c

--- src/crc64.c
+++ src/crc64.c
@@ -39,6 +39,9 @@
 
 #include <stdint.h>
 
+/* UINT64_C
+ * #define UINT64_C(x) (x##ull)
+ */
 static const uint64_t crc64_tab[256] = {
     UINT64_C(0x0000000000000000), UINT64_C(0x7ad870c830358979),
     UINT64_C(0xf5b0e190606b12f2), UINT64_C(0x8f689158505e9b8b),
